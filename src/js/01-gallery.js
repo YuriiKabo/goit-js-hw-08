5b1319275a78f959d8cfc37da704d1dc539caeef
YuriiKabo <yourrocksmirnov@gmail.com>
2022-11-18 20:32:39 +0200

    repack hw8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,26 @@
-import { galleryItems } from "./gallery-items";
+import { galleryItems } from "./gallery-items.js";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const galleryItemsMarkup = galleryItems
+  .map(
+    (img) =>
+      `<div class="gallery__item">
+      <a class="gallery__link" href=${img.original}>
+      <img
+      class="gallery__image"
+      src=${img.preview}
+      alt=${img.description}
+    />
+    </a>
+    </div>`
+  )
+  .join("");
+galleryList.insertAdjacentHTML("afterbegin", galleryItemsMarkup);
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  disableRightClick: true,
+});
