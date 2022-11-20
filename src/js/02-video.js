f814a6cb2ec81b8fab6cbf919b23d566d11ef368
YuriiKabo <yourrocksmirnov@gmail.com>
2022-11-20 18:02:43 +0200

    Hw8 task2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(
+    (onPlayUpdate = evt => {
+      localStorage.setItem('videoplayer-current-time', evt.seconds);
+    }),
+    1000
+  )
+);
+
+const currentTime = localStorage.getItem('videoplayer-current-time')
+  ? localStorage.getItem('videoplayer-current-time')
+  : 0;
+
+player.setCurrentTime(currentTime);
