1a13ed624a7f1c783934f14752b96d6172e3bccc
YuriiKabo <yourrocksmirnov@gmail.com>
2022-11-22 17:27:12 +0200

    hw8 fix bugs is done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,33 +1,45 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-const textarea = document.querySelector('.feedback-form textarea');
 const input = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
-const formDataObj = {};
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onInput, 500));
 
-populateData();
-
 function onInput(e) {
+  const formDataObj = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
   formDataObj[e.target.name] = e.target.value;
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formDataObj));
 }
 
+// const savedDataObj = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+const saveLocalItems = localStorage.getItem(LOCALSTORAGE_KEY);
+const savedDataObj = JSON.parse(saveLocalItems);
+
 function populateData() {
-  const savedData = localStorage.getItem(LOCALSTORAGE_KEY);
-  const savedDataObj = JSON.parse(savedData);
-  if (savedData) {
-    input.value = savedDataObj.email;
-    textarea.value = savedDataObj.message;
+  if (saveLocalItems) {
+    !savedDataObj.email ? '' : (input.value = savedDataObj.email);
+    !savedDataObj.message ? '' : (textarea.value = savedDataObj.message);
   }
 }
 
+populateData();
+
 function onFormSubmit(evt) {
   evt.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = evt.currentTarget;
+
+  if (email.value === '' || message.value === '') {
+    return alert('Заповнені не всі поля!!!');
+  }
+  const formOutput = { email: input.value, message: textarea.value };
+  console.log(formOutput);
+
   evt.currentTarget.reset();
   localStorage.removeItem(LOCALSTORAGE_KEY);
-  console.log(formDataObj);
 }
